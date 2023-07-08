c7a71fad56f30b0b874b5e166c0da71263527a3a
Aryan Sharma <50998117+aryan2909@users.noreply.github.com>
2023-07-08 19:08:41 +0530

    Time: 65 ms (19.46%), Space: 17.6 MB (45.52%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return False
+
+
+        if root.left is None and root.right is None and root.val == targetSum:
+            return True
+
+        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
